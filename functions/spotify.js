78eb6ef3d052716cba715601fb280265166863dd
James Keicher <jamesdkeicher@gmail.com>
2022-02-28 17:23:41 -0800

    feat: fetch playlist and render audio players for each song in playlist

--- functions/spotify.js
+++ functions/spotify.js
@@ -1,20 +1,25 @@
-
-require('dotenv').config();const fetch = require('node-fetch');
-
+require('dotenv').config();
+const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
   const baseUrl = 'https://api.spotify.com/v1';
-  const playlist = baseUrl + `/playlists/6PiZPsIotykZkKx2GiZK4H/tracks`;
+  const playlist = baseUrl + `/playlists/6PiZPsIotykZkKx2GiZK4H/tracks?market=US`;
   try {
-    const response = await fetch(playlist, { headers: { Accept: 'application/json', ['Content-Type']: 'application/json', Authorization: `Bearer BQASKq8zqZz5g9tLzkFV99EQKXQIPFuwcD161mQLJXuN2XxRxJWKHai9itYeTo8PBOZFMyAkGc6iDbA15Cs` } });
+    const response = await fetch(playlist, {
+      headers: {
+        Accept: 'application/json',
+        ['Content-Type']: 'application/json',
+        Authorization: `Bearer BQASKq8zqZz5g9tLzkFV99EQKXQIPFuwcD161mQLJXuN2XxRxJWKHai9itYeTo8PBOZFMyAkGc6iDbA15Cs`,
+      },
+    });
     const data = await response.json();
     const json = JSON.stringify({ data });
 
     console.log(json);
-    
-    return { 
-      statusCode: 200, 
-      body: json
+
+    return {
+      statusCode: 200,
+      body: json,
     };
   } catch (error) {
     console.log(error);
--- src/SpotifyPlaylist.js
+++ src/SpotifyPlaylist.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function SpotifyPlaylist() {
+  const [playlists, setPlaylists] = useState([]);
+
   const netlifyUrl = '/.netlify/functions/spotify';
 
-  async function fetchPlayLists() {
-    const response = await fetch(netlifyUrl);
-    console.log(response);
-    // const json = await response.json();
-  }
-  fetchPlayLists();
+  useEffect(() => {
+    async function fetchPlayLists() {
+      const response = await fetch(netlifyUrl);
+      const json = await response.json();
+      console.log(json);
+      setPlaylists(json.data.items);
+    }
+    fetchPlayLists();
+  }, []);
+
   return (
-    <div>SpotifyPlaylist</div>
+    <div>
+      {playlists.map((playlistItem, i) => {
+        return <audio key={i} src={playlistItem.track.preview_url} controls></audio>;
+      })}
+    </div>
   );
 }
