24c11c6224e0333a351f5383c51c85634101467e
Clayton Knapp <csjknapp@gmail.com>
2022-03-02 18:20:37 -0800

    cleaned up game page

--- src/GamePage.jsx
+++ src/GamePage.jsx
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { insertLeaderBoard } from './services/fetch-utils';
+import './GamePage.css';
 
 export default function GamePage({ token, userProfile }) {
-  console.log(userProfile);
-  const netlifyUrl = '/.netlify/functions/spotify-playlist-items';
   const [tracks, setTracks] = useState(null);
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [userGuess, setUserGuess] = useState('');
@@ -17,17 +16,30 @@ export default function GamePage({ token, userProfile }) {
   const [availablePoints, setAvailablePoints] = useState(100);
   const [pointsTimer, setPointsTimer] = useState('');
   const [isCorrectGuess, setIsCorrectGuess] = useState(false);
+
+  const netlifyUrl = '/.netlify/functions/spotify-playlist-items';
+
   const params = useParams();
   const history = useHistory();
 
   function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
       array[i] = array[j];
       array[j] = temp;
     }
   }
+
+  //STRETCH FEATURE
+  // CREATE AN ARRAY OF 4 CHOICE
+  // 1 is the correct answer
+  // 3 are random incorrect answers
+  // So we could push the correct answer into an array
+  // Then randomly push 3 more that DO NOT MATCH THE FIRST/CORRECT ANSWER
+  //THEN RANDOMIZE THE WHOLE ARRAY
+
+
   useEffect(() => {
     if (tracks) {
       let trackShuffled = [...tracks];
@@ -45,6 +57,46 @@ export default function GamePage({ token, userProfile }) {
     fetchPlayListsFromSpotify();
   }, [token, params.id]);
 
+  useEffect(() => {
+    !isGameStarted && clearInterval(timer);
+    !isGameStarted && clearInterval(pointsTimer);
+  }, [isGameStarted]);
+  
+
+  
+  function handleStartGame() {
+    setIsGameStarted(true);
+    setTimer(setInterval(decrementTimer, 1000));
+    setPointsTimer(setInterval(decrementPoints, 5000));
+  }
+  
+  async function handleSubmit(e) {
+    e.preventDefault();
+    
+    setIsGameStarted(false);
+    
+    if (userGuess === tracks[counter].track.id) {
+      setIsCorrectGuess(true);
+      setTotalPoints(totalPoints + availablePoints);
+    } else {
+      setIsCorrectGuess(false);
+    }
+    
+    setCounter(counter + 1);
+    
+    setCountDownSeconds(30);
+    setAvailablePoints(100);
+    setUserGuess('');
+    counter + 1 === tracks.length &&
+    (await insertLeaderBoard({
+      username: userProfile.username,
+      user_id: userProfile.user_id,
+      score: totalPoints,
+      rounds: tracks.length,
+    }));
+  }
+  
+
   const handleLeaderboardClick = () => {
     history.push('/leaderboard');
   };
@@ -52,55 +104,22 @@ export default function GamePage({ token, userProfile }) {
   const handleChooseNewGameClick = () => {
     history.push('/selection');
   };
+  
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setIsGameStarted(false);
-    if (userGuess === tracks[counter].track.id) {
-      setIsCorrectGuess(true);
-      setTotalPoints(totalPoints + availablePoints);
-    } else {
-      setIsCorrectGuess(false);
-    }
-    setCounter(counter + 1);
-
-    setCountDownSeconds(30);
-    setAvailablePoints(100);
-    counter + 1 === tracks.length &&
-      (await insertLeaderBoard({
-        username: userProfile.username,
-        user_id: userProfile.user_id,
-        score: totalPoints,
-        rounds: tracks.length,
-      }));
-    setUserGuess('');
-  }
-
-  function handleStartGame() {
-    setIsGameStarted(true);
-    setTimer(setInterval(decrementAndDisplayTimer, 1000));
-
-    setPointsTimer(setInterval(decrementPoints, 5000));
-  }
-
-  function decrementAndDisplayTimer() {
+  function decrementTimer() {
     setCountDownSeconds((countDownSeconds) => countDownSeconds - 1);
   }
-
+  
   function decrementPoints() {
     setAvailablePoints((availablePoints) => availablePoints - 10);
   }
 
-  useEffect(() => {
-    !isGameStarted && clearInterval(timer);
-    !isGameStarted && clearInterval(pointsTimer);
-  }, [isGameStarted]);
 
   return (
-    <div>
-      Welcome to GamePage
+    <div className='game-page'>
+      <h2>Welcome to Tunify! Now name that tune!</h2>
       {!isGameStarted && <button onClick={handleStartGame}>Begin Round</button>}
-      {counter !== 0 && <h2>{isCorrectGuess ? 'CORRECT!!!!' : 'BUMMER YOU FAILED:('}</h2>}
+      {counter !== 0 && <h2>{isCorrectGuess ? 'CORRECT!!!!' : 'WRONG ANSWER!'}</h2>}
       {counter === tracks?.length ? (
         <div className="completed-game-state">
           <p>{`CONGRATS YOU'VE COMPLETED ${tracks?.length} ROUNDS. Your total points were ${totalPoints}. Great job you nerd!`}</p>
@@ -123,20 +142,20 @@ export default function GamePage({ token, userProfile }) {
         </div>
       )}
       <form onSubmit={handleSubmit}>
-        <label name="userGuess" className="track">
+        <div name="userGuess" className="choices">
           {tracks &&
             tracksShuffled.map((track, i) => (
-              <div key={track + i}>
-                <p>{track.track.name}</p>
+              <div key={track + i} className='radio'>
                 <input
                   onChange={(e) => setUserGuess(e.target.value)}
                   type="radio"
                   name="userGuess"
                   value={track.track.id}
                 />
+                <p>{track.track.name}</p>
               </div>
             ))}
-        </label>
+        </div>
         {userGuess ? <button>Submit Guess</button> : <button>Skip</button>}
       </form>
     </div>
