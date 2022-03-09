d4e34f9b480210064a2123890b32aff9edc2a36d
James Keicher <jamesdkeicher@gmail.com>
2022-03-09 13:37:14 -0800

    Construct playlist from series of dropdown events. User can choose value in dropdown, available tracks are displayed, user can select track to go in playlist, user can change dropdown value to get new list of tracks and continue building playlist without state loss of new playlist

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Profile({ token, userProfile }) {
-  const [newPlaylist, setNewPlaylist] = useState([]);
+  const [newPlaylist, setNewPlaylist] = useState('');
   const [selectedPlaylist, setSelectedPlaylist] = useState('');
   const [featuredPlaylistTracks, setFeaturedPlaylistTracks] = useState([]);
   const [options, setOptions] = useState([]);
-  const [isChecked, setIsChecked] = useState(false);
+  //   const [isChecked, setIsChecked] = useState(false);
   const [checkedState, setCheckedState] = useState([]);
 
   const netlifyUrl = '/.netlify/functions/spotify-featured-playlist';
@@ -14,7 +14,6 @@ export default function Profile({ token, userProfile }) {
     async function fetchFeaturedPlayListsFromSpotify() {
       const response = await fetch(`${netlifyUrl}?token=${token}`);
       const json = await response.json();
-      console.log(json.data.playlists.items);
       setOptions(json.data.playlists.items);
       setSelectedPlaylist(json.data.playlists.items[0].id);
     }
@@ -22,7 +21,6 @@ export default function Profile({ token, userProfile }) {
   }, [token]);
 
   useEffect(() => {
-    console.log(options);
     setCheckedState(new Array(options.length).fill(false));
   }, [options]);
 
@@ -37,21 +35,36 @@ export default function Profile({ token, userProfile }) {
         (await json.data.items.map((item) => {
           item.track.preview_url && tracks.push(item);
         }));
-      console.log(tracks);
       setFeaturedPlaylistTracks(tracks);
     };
-    displayTracksList();
+    selectedPlaylist && displayTracksList();
   }, [selectedPlaylist]);
 
-  const handleCheckboxChange = (position) => {
+  const handleCheckboxChange = async (position) => {
     const updatedCheckedState = checkedState.map((item, index) =>
       index === position ? !item : item
     );
     setCheckedState(updatedCheckedState);
+
+    const chosenTracks = await updatedCheckedState.reduce((acc, currentState, index) => {
+      if (currentState === true) {
+        const newTrack = featuredPlaylistTracks[index];
+        return acc.concat(newTrack);
+      }
+      return acc;
+    }, []);
+    console.log(chosenTracks);
+    const currentPlaylist = [...newPlaylist, ...chosenTracks];
+    setNewPlaylist(currentPlaylist);
   };
   return (
     <div className="playlist-creation-panel">
-      <select onChange={(e) => setSelectedPlaylist(e.target.value)}>
+      <select
+        onChange={(e) => {
+          setSelectedPlaylist(e.target.value);
+          setCheckedState(new Array(options.length).fill(false));
+        }}
+      >
         {options.map((option, i) => {
           return (
             <option key={option.name + i} value={option.id}>
@@ -64,10 +77,29 @@ export default function Profile({ token, userProfile }) {
         <div className="playlist-track-options tracks">
           {featuredPlaylistTracks &&
             featuredPlaylistTracks.map((track, i) => {
+              return (
+                <div key={track.track.name + i}>
+                  <input
+                    key={track.track.name + i}
+                    type="checkbox"
+                    id={`custom-checkbox-${i}`}
+                    name={track.track.name}
+                    value={track.track.name}
+                    checked={checkedState[i]}
+                    onChange={() => handleCheckboxChange(i)}
+                  ></input>
+                  <label>{track.track.name}</label>
+                </div>
+              );
+            })}
+        </div>
+        <div className="user-selected-tracks tracks">
+          {newPlaylist.length > 0 &&
+            newPlaylist.map((track, i) => {
+              console.log(newPlaylist);
               return <p key={track.track.name + i}>{track.track.name}</p>;
             })}
         </div>
-        <div className="user-selected-tracks tracks"></div>
       </div>
     </div>
   );
